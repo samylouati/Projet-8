645f9a7eb7e3e02fe2b7213bd337c396edb6f57b
samylouati <148778977+samylouati@users.noreply.github.com>
2024-03-25 12:42:05 +0100

    debut Header

--- KASA/src/components/header/header.jsx
+++ KASA/src/components/header/header.jsx
@@ -2,6 +2,10 @@ export function Header () {
     return (
         <header>
             <h1><img src="" alt="" /></h1>
+            <nav>
+                <a href="">Accueil</a>
+                <a href="">A propos</a>
+            </nav>
         </header>
     );
 }
